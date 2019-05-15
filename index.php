bb92b9c3789b600eadbffe287c02f50064641a0c
hermawanwirasantoso <44387507+hermawanwirasantoso@users.noreply.github.com>
2019-05-15 23:30:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ if (isset($_POST['submit'])) {
     // echo fread($content, filesize($fileToUpload));
     $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
 
-    header("Location: upload.php?img=$url");
+    header("Location: index.php?img=$url");
 }
 ?>
 
@@ -173,7 +173,7 @@ if (isset($_POST['submit'])) {
                 <h1>Upload Foto</h1>
             </div>
         <div class="mt-4 mb-2">
-            <form action="upload.php" method="post" enctype="multipart/form-data">
+            <form action="index.php" method="post" enctype="multipart/form-data">
                 <input type="file" name="fileToUpload" accept=".jpeg,.jpg,.png" required>
                 <input type="submit" name="submit" value="Upload">
             </form>
